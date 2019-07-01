4189cdafa5da0e1fb86e7cbbae9116c08201fcfb
Loïc Colat <loic.colat@gmail.com>
2019-07-01 13:57:16 +0200

    adapt arlonx code

--- Program.cs
+++ Program.cs
@@ -1,12 +1,38 @@
 ﻿using System;
+using el_chupitos_pattern.payment;
 
-namespace el_chupitos_pattern
+namespace payement
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            PaymentMethod payementMethod = new PaymentMethod();
+
+            Console.WriteLine("Indiquez le prix");
+            var method = Console.ReadLine();
+            payementMethod.setMethod(method);
+
+            Console.WriteLine("Quel type de payement utiliser?");
+            int input = int.Parse(Console.ReadKey().KeyChar.ToString());
+
+            switch(input){
+                case 1:
+                    payementMethod.setPaymentStrategy(new Cash());
+                    payementMethod.Payement();
+                    break;
+                case 2:
+                    payementMethod.setPaymentStrategy(new CreditCard());
+                    payementMethod.Payement();
+                    break;
+                case 3:
+                    payementMethod.setPaymentStrategy(new Cheque());
+                    payementMethod.Payement();
+                    break;
+                default:
+                    Console.WriteLine("Selection invalide");
+                    break;
+            }
         }
     }
 }
---
+++ payment/payementMethod.cs
@@ -0,0 +1,44 @@
+using System;
+
+namespace el_chupitos_pattern.payment
+{
+    abstract class PaymentStrategy{
+        public abstract void Payement(string method);
+    }
+
+    class Cash : PaymentStrategy{
+        public override void Payement(string method){
+            Console.WriteLine("\nPayement " + method + "€ par cash");
+        }
+    }
+
+    class CreditCard : PaymentStrategy{
+        public override void Payement(string method){
+            Console.WriteLine("\nPayement " + method + "€ par CB");
+        }
+    }
+
+    class Cheque : PaymentStrategy{
+        public override void Payement(string method){
+            Console.WriteLine("\nPayement " + method + "€ par cheque");
+        }
+    }
+
+    class PaymentMethod{
+        private string Method;
+        private PaymentStrategy _paymentStrategy;
+
+        public void setPaymentStrategy(PaymentStrategy paymentStrategy){
+            this._paymentStrategy = paymentStrategy;
+        }
+
+        public void setMethod(string name){
+            Method = name;
+        }
+
+        public void Payement(){
+            _paymentStrategy.Payement(Method);
+            Console.WriteLine();
+        }
+    }
+}
